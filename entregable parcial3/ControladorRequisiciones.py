684082ba2920e0c48f90916b386e4d63bb358f48
adrianastyles <125921265+adrianastyles@users.noreply.github.com>
2023-04-05 20:21:59 -0600

    terminación del entregable parcial 3

--- entregable parcial3/ControladorRequisiciones.py
+++ entregable parcial3/ControladorRequisiciones.py
@@ -34,3 +34,199 @@ class controladorRequi:
             conx.close
             messagebox.showinfo("Registro exitoso", "Requisición guardada")
             
+    def consultarRequi(self, id):
+        #1. Realizar conexión a la base de datos
+        conx = self.conexionBD()
+        
+        #2. Verificar que el id no esté vacío
+        if (id == ""):
+            messagebox.showwarning ("Warning", "Ingresa un ID")
+            conx.close()
+        else:
+            #3. Ejecutar la consulta
+            try: 
+                #4. Preparar lo necesario
+                cursor = conx.cursor()
+                sqlSelect = "select * from Requisicion where id_requi = " + id
+                
+                #5. Ejecutar y cerrar conexión
+                cursor.execute(sqlSelect)
+                RSusuario = cursor.fetchall()
+                conx.close()
+                return RSusuario
+                
+            except sqlite3.OperationalError:
+                print ("Error de consulta")
+    
+    def consultarTodosLosUsuarios(self):
+        #1. Realizar conexión a la base de datos
+        conx = self.conexionBD()
+        
+        try: 
+                #4. Preparar lo necesario
+            cursor = conx.cursor()
+            sqlSelect = "select * from Requisicion"
+                
+                #5. Ejecutar y cerrar conexión
+            cursor.execute(sqlSelect)
+            RSusuario = cursor.fetchall()
+            conx.close()
+            return RSusuario
+                
+        except sqlite3.OperationalError:
+            print ("Error de consulta todos los usuarios")
+            
+    def EliminarRequi(self, id):
+        conx = self.conexionBD()
+        
+        #2. Verificar que el id no esté vacío
+        if (id == ""):
+            messagebox.showwarning ("Warning", "Ingresa un ID")
+            conx.close()
+        else:
+            #3. Ejecutar la consulta
+            try: 
+                #4. Preparar lo necesario
+                cursor = conx.cursor()
+                sqlSelect = "DELETE FROM Requisicion WHERE id_requi = "+ id
+                
+                #5. Ejecutar y cerrar conexión
+                cursor.execute(sqlSelect)
+                conx.commit()
+                conx.close()
+                
+            except sqlite3.OperationalError:
+                print ("Error")
+    
+    def ActualizarRequiNum(self, id, requi):
+        conx = self.conexionBD()
+        
+        #2. Verificar que el id no esté vacío
+        if (id == "" or requi == ""):
+            messagebox.showwarning ("Warning", "Ingresa el ID y número de requisición")
+            conx.close()
+        else:
+            #3. Ejecutar la consulta
+            try: 
+                #4. Preparar lo necesario
+                cursor = conx.cursor()
+                sqlSelect = "UPDATE Requisicion SET requisicion = ? WHERE id_requi = ?"
+                actualizar = (requi, id)
+                #5. Ejecutar y cerrar conexión
+                cursor.execute(sqlSelect, actualizar)
+                conx.commit()
+                conx.close()
+                
+            except sqlite3.OperationalError:
+                print ("Error para actualizar requisición")
+    
+    def ActualizarFecha(self, id, fecha):
+        conx = self.conexionBD()
+        
+        #2. Verificar que el id no esté vacío
+        if (id == "" or fecha == ""):
+            messagebox.showwarning ("Warning", "Ingresa el ID y fecha")
+            conx.close()
+        else:
+            #3. Ejecutar la consulta
+            try: 
+                #4. Preparar lo necesario
+                cursor = conx.cursor()
+                sqlSelect = "UPDATE Requisicion SET fecha = ? WHERE id_requi = ?"
+                actualizar = (fecha, id)
+                #5. Ejecutar y cerrar conexión
+                cursor.execute(sqlSelect, actualizar)
+                conx.commit()
+                conx.close()
+                
+            except sqlite3.OperationalError:
+                print ("Error para actualizar fecha")
+    
+    def ActualizarPartida(self, id, part):
+        conx = self.conexionBD()
+        
+        #2. Verificar que el id no esté vacío
+        if (id == "" or part== ""):
+            messagebox.showwarning ("Warning", "Ingresa el ID y partida")
+            conx.close()
+        else:
+            #3. Ejecutar la consulta
+            try: 
+                #4. Preparar lo necesario
+                cursor = conx.cursor()
+                sqlSelect = "UPDATE Requisicion SET partida = ? WHERE id_requi = ?"
+                actualizar = (part, id)
+                #5. Ejecutar y cerrar conexión
+                cursor.execute(sqlSelect, actualizar)
+                conx.commit()
+                conx.close()
+                
+            except sqlite3.OperationalError:
+                print ("Error para actualizar partida")
+    
+    def ActualizarCantidad(self, id, cant):
+        conx = self.conexionBD()
+        
+        #2. Verificar que el id no esté vacío
+        if (id == "" or cant == ""):
+            messagebox.showwarning ("Warning", "Ingresa el ID y cantidad")
+            conx.close()
+        else:
+            #3. Ejecutar la consulta
+            try: 
+                #4. Preparar lo necesario
+                cursor = conx.cursor()
+                sqlSelect = "UPDATE Requisicion SET cantidad = ? WHERE id_requi = ?"
+                actualizar = (cant, id)
+                #5. Ejecutar y cerrar conexión
+                cursor.execute(sqlSelect, actualizar)
+                conx.commit()
+                conx.close()
+                
+            except sqlite3.OperationalError:
+                print ("Error para actualizar requisición")
+                
+    def ActualizarDescr(self, id, descr):
+        conx = self.conexionBD()
+        
+        #2. Verificar que el id no esté vacío
+        if (id == "" or descr == ""):
+            messagebox.showwarning ("Warning", "Ingresa el ID y descripción")
+            conx.close()
+        else:
+            #3. Ejecutar la consulta
+            try: 
+                #4. Preparar lo necesario
+                cursor = conx.cursor()
+                sqlSelect = "UPDATE Requisicion SET descripcion = ? WHERE id_requi = ?"
+                actualizar = (descr, id)
+                #5. Ejecutar y cerrar conexión
+                cursor.execute(sqlSelect, actualizar)
+                conx.commit()
+                conx.close()
+                
+            except sqlite3.OperationalError:
+                print ("Error para actualizar descripción")
+                
+    def ActualizarASoli(self, id, soli):
+        conx = self.conexionBD()
+        
+        #2. Verificar que el id no esté vacío
+        if (id == "" or soli == ""):
+            messagebox.showwarning ("Warning", "Ingresa el ID y área del solicitante")
+            conx.close()
+        else:
+            #3. Ejecutar la consulta
+            try: 
+                #4. Preparar lo necesario
+                cursor = conx.cursor()
+                sqlSelect = "UPDATE Requisicion SET areaSolicitante = ? WHERE id_requi = ?"
+                actualizar = (soli, id)
+                #5. Ejecutar y cerrar conexión
+                cursor.execute(sqlSelect, actualizar)
+                conx.commit()
+                conx.close()
+                
+            except sqlite3.OperationalError:
+                print ("Error para actualizar área solicitante")
+            
--- entregable parcial3/inicio.py
+++ entregable parcial3/inicio.py
@@ -7,10 +7,58 @@ controladorR = controladorRequi()
 
 def GuardarRequi():
     controladorR.GuardarRequisicion(varRequi.get(), varFecha.get(), varPartida.get(), varCantidad.get(), varDescr.get(), varASolicitante.get())
+    
+def BuscarRequi():
+    buscar = controladorR.consultarRequi(varIDCon.get())
+    if (buscar):
+        for usu in buscar:
+            tabla.delete(*tabla.get_children())
+            tabla.insert("", "end", text = usu[0], values = (usu[1], usu[2], usu[3], usu[4], usu[5], usu[6]))
+    else:
+        messagebox.showwarning ("No encontrado", "La requisición no existe en la base de datos", icon = "error")
+        
+def ConsultarRequi():
+    
+    bus = controladorR.consultarTodosLosUsuarios()
+    tabla1.delete(*tabla1.get_children()) #limpia
+    for usu in bus:
+        tabla1.insert("", "end", text = usu[0], values = (usu[1], usu[2], usu[3], usu[4], usu[5], usu[6]))
+
+def EliminarRequi():
+    msg = messagebox.askquestion("Warning", "¿Está seguro de eliminar la requisición?")
+    if (msg == "no"):
+        messagebox.showinfo("No eliminado", "Requisición no eliminada")
+    else:
+        controladorR.EliminarRequi(varDelete.get())
+        messagebox.showinfo("Eliminado exitoso", "Requisición eliminada")
+        
+def ActualizarRequi():
+    controladorR.ActualizarRequiNum(varID.get(), varRequi1.get())
+    messagebox.showinfo("Actualización exitosa", "Se actualizó el número de requisición exitosamente")
+
+def ActualizarFecha():
+    controladorR.ActualizarFecha(varID.get(), varFecha1.get())
+    messagebox.showinfo("Actualización exitosa", "Se actualizó la fecha exitosamente")
+
+def ActualizarPartida():
+    controladorR.ActualizarPartida(varID.get(), varPartida1.get())
+    messagebox.showinfo("Actualización exitosa", "Se actualizó la partida exitosamente")
+    
+def ActualizarCantidad():
+    controladorR.ActualizarCantidad(varID.get(), varCantidad1.get())
+    messagebox.showinfo("Actualización exitosa", "Se actualizó la cantidad exitosamente")
+
+def ActualizarDescr():
+    controladorR.ActualizarDescr(varID.get(), varDescr1.get())
+    messagebox.showinfo("Actualización exitosa", "Se actualizó la descripción exitosamente")
+
+def ActualizarASoli():
+    controladorR.ActualizarASoli(varID.get(), varASolicitante1.get())
+    messagebox.showinfo("Actualización exitosa", "Se actualizó el área del solicitante exitosamente")
 
 Ventana = Tk()
 Ventana.title("REQUISICIONES")
-Ventana.geometry("500x350")
+Ventana.geometry("500x400")
 
 panel = ttk.Notebook(Ventana)
 panel.pack(fill = 'both', expand = 'yes')
@@ -19,6 +67,9 @@ pestana1 = ttk.Frame(panel)
 pestana2 = ttk.Frame(panel)
 pestana3 = ttk.Frame(panel)
 pestana4 = ttk.Frame(panel)
+pestana5 = ttk.Frame(panel)
+
+#PESTAÑA 1
 
 varRequi = tk.StringVar()
 varFecha = tk.StringVar()
@@ -48,9 +99,99 @@ txtASoli = Entry(pestana1, textvariable = varASolicitante).pack()
 
 btnGuardar = Button(pestana1, text = "Guardar requisición", command = GuardarRequi).pack()
 
+#PESTAÑA 2
+
+varIDCon = tk.StringVar()
+titulo = Label(pestana2, text = "Consultar requisición", fg = "blue", font = ("Modern", 18)). pack()
+lblID = Label(pestana2, text = "ID de requisicion:").pack()
+txtID = Entry(pestana2, textvariable = varIDCon).pack()
+btnBusqueda = Button(pestana2, text = "Buscar", command = BuscarRequi).pack()
+subID = Label(pestana2, text = "Registrado:", fg = "blue", font = ("Modern",15)).pack()
+tabla = ttk.Treeview (pestana2)
+tabla ["columns"] = ("Requisición", "Fecha", "Partida", "Cantidad", "Descripción", "Área solicitante")
+tabla.column("#0", width = 60, minwidth = 60)
+tabla.column("Requisición", width = 100, minwidth = 60)
+tabla.column("Fecha", width = 200, minwidth = 110)
+tabla.column("Partida", width = 100, minwidth = 60)
+tabla.column("Cantidad", width = 100, minwidth = 60)
+tabla.column("Descripción", width = 100, minwidth = 200)
+tabla.column("Área solicitante", width = 100, minwidth = 200)
+tabla.heading("#0", text = "ID", anchor = tk.CENTER)
+tabla.heading("Requisición", text = "Requisición", anchor = tk.CENTER)
+tabla.heading("Fecha", text = "Fecha", anchor = tk.CENTER)
+tabla.heading("Partida", text = "Partida", anchor = tk.CENTER)
+tabla.heading("Cantidad", text = "Cantidad", anchor = tk.CENTER)
+tabla.heading("Descripción", text = "Descripción", anchor = tk.CENTER)
+tabla.heading("Área solicitante", text = "Área solicitante", anchor = tk.CENTER)
+tabla.pack()
+
+# PESTAÑA 3
+titulo = Label(pestana3, text = "Consultar requisición", fg = "blue", font = ("Modern", 18)). pack()
+subID1 = Label(pestana3, text = "Registrados:", fg = "blue", font = ("Modern",15)).pack()
+tabla1 = ttk.Treeview (pestana3)
+tabla1 ["columns"] = ("Requisición", "Fecha", "Partida", "Cantidad", "Descripción", "Área solicitante")
+tabla1.column("#0", width = 60, minwidth = 60)
+tabla1.column("Requisición", width = 100, minwidth = 60)
+tabla1.column("Fecha", width = 200, minwidth = 110)
+tabla1.column("Partida", width = 100, minwidth = 60)
+tabla1.column("Cantidad", width = 100, minwidth = 60)
+tabla1.column("Descripción", width = 100, minwidth = 200)
+tabla1.column("Área solicitante", width = 100, minwidth = 200)
+tabla1.heading("#0", text = "ID", anchor = tk.CENTER)
+tabla1.heading("Requisición", text = "Requisición", anchor = tk.CENTER)
+tabla1.heading("Fecha", text = "Fecha", anchor = tk.CENTER)
+tabla1.heading("Partida", text = "Partida", anchor = tk.CENTER)
+tabla1.heading("Cantidad", text = "Cantidad", anchor = tk.CENTER)
+tabla1.heading("Descripción", text = "Descripción", anchor = tk.CENTER)
+tabla1.heading("Área solicitante", text = "Área solicitante", anchor = tk.CENTER)
+tabla1.pack()
+btnBusqueda = Button(pestana3, text = "Buscar", command = ConsultarRequi).pack()
+
+# PESTAÑA 4
+
+varID = tk.StringVar()
+varRequi1 = tk.StringVar()
+varFecha1 = tk.StringVar()
+varPartida1 = tk.StringVar()
+varCantidad1 = tk.StringVar()
+varDescr1 = tk.StringVar()
+varASolicitante1 = tk.StringVar()
+
+titulo4 = Label(pestana4, text = "Actualizar usuarios", fg = "blue", font = ("Modern", 18)). pack()
+lblID2 = Label(pestana4, text = "ID requisición:").pack()
+txtID2 = Entry(pestana4, textvariable = varID).pack()
+lblRequi1 = Label(pestana4, text = "Requisicion:").pack()
+txtRequi1 = Entry(pestana4, textvariable = varRequi1).pack()
+btnRequi1 = Button(pestana4, text = "Actualizar número requisición", command = ActualizarRequi).pack()
+lblFecha1 = Label(pestana4, text = "Fecha:").pack()
+txtFecha1 = Entry(pestana4, textvariable = varFecha1).pack()
+btnFecha1 = Button(pestana4, text = "Actualizar fecha", command = ActualizarFecha).pack()
+lblPartida1 = Label(pestana4, text = "Partida:").pack()
+txtPartida1 = Entry(pestana4, textvariable = varPartida1).pack()
+btnPartida1 = Button(pestana4, text = "Actualizar partida", command = ActualizarPartida).pack()
+lblCantidad1 = Label(pestana4, text = "Cantidad:").pack()
+txtCantidad1 = Entry(pestana4, textvariable = varCantidad1).pack()
+btnCantidad1 = Button(pestana4, text = "Actualizar cantidad", command = ActualizarCantidad).pack()
+lblDescr1 = Label(pestana4, text = "Descripción:").pack()
+txtDescr1 = Entry(pestana4, textvariable = varDescr1).pack()
+btnDescr1 = Button(pestana4, text = "Actualizar descripción", command = ActualizarDescr).pack()
+lblASoli1 = Label(pestana4, text = "Área del solicitante:").pack()
+txtASoli1 = Entry(pestana4, textvariable = varASolicitante1).pack()
+btnASoli1 = Button(pestana4, text = "Actualizar área solicitante", command = ActualizarASoli).pack()
+
+# PESTAÑA 5
+
+varDelete = tk.StringVar()
+titulo5 = Label(pestana5, text = "Eliminar requisición", fg = "blue", font = ("Modern", 18)). pack()
+lblID2 = Label(pestana5, text = "Identificador de requisición:").pack()
+txtID2 = Entry(pestana5, textvariable = varDelete).pack()
+btnEliminar = Button(pestana5, text = "Eliminar", command = EliminarRequi).pack()
+
+
 panel.add(pestana1, text = 'Formulario')
 panel.add(pestana2, text = 'Buscar Requisición')
-panel.add(pestana3, text = 'Consultar Requisición')
+panel.add(pestana3, text = 'Consultar Requisiciones')
 panel.add(pestana4, text = 'Actualizar Requisición')
+panel.add(pestana5, text = 'Eliminar Requisición')
 
 Ventana.mainloop()
